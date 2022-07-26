7a629d9f81e4a29f96a9a677f5d5f58e82608cdc
Arnau Mas <57038668+Arini11@users.noreply.github.com>
2022-07-26 11:48:23 +0200

    Update ex02.sql

--- ex02.sql
+++ ex02.sql
@@ -16,3 +16,10 @@ SELECT a.nombre,a.apellidos FROM empleados AS a
     WHERE b.presupuesto > 60000;
 SELECT * FROM departamentos WHERE presupuesto > (SELECT avg(presupuesto) FROM departamentos);
 SELECT nombre FROM departamentos WHERE codigo IN ( SELECT departamento FROM empleados GROUP BY departamento HAVING COUNT(*) > 2 );
+INSERT INTO departamentos VALUES(11, 'Calidad', 40000);
+INSERT INTO empleados VALUES(89267109, 'Esther', 'Vázquez', 11);
+UPDATE departamentos SET presupuesto = presupuesto*0.9;
+UPDATE empleados SET departamento = 14 WHERE departamento = 77;
+DELETE FROM empleados WHERE departamento = 14;
+DELETE FROM empleados WHERE departamento IN (SELECT codigo FROM departamentos WHERE presupuesto > 60000);
+DELETE FROM empleados;
